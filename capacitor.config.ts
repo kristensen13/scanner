ee4db9b1efd9cf3997bee34351a9b8f154179bc5
Wilson Loayza Coello <wilsonloayzacoello@iMac-de-Wilson.local>
2024-11-02 23:52:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scanner-EAN',
+  webDir: 'www'
+};
+
+export default config;
